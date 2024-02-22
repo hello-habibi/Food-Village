7ad5c2773f2fecd612e4f5c7ebde894dcf1bc076
EmonThePrince <saimahmedemon7@gmail.com>
2024-02-22 18:44:17 +0600

    First by Emon

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-gray-700 text-white flex justify-center text-xl'>Hello</div>
+    </>
+  )
+}
+
+export default App
